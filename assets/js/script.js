0ed8554f394dad732f18b05d9d431eb7bca5e9c8
Alejandra <informaticotrabajo00@gmail.com>
2024-02-25 20:39:33 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+$(document).ready(function () {
+  //funcion que valida el numero del heroe
+  function validar(num) {
+    let pasamosLaValidacion = true; // inicializa el interruptor para validacion true o false
+    let validacionNumero = /^[0-9]+$/; // patron para solo aceptar numeros positivos
+    if (validacionNumero.test(num) === false) {
+      pasamosLaValidacion = false;
+      limpiar();
+      alert("Ingrese un numero entre 1 y 731");
+    }
+    return pasamosLaValidacion;
+  }
+  //funcion para limppiar las secciones donde mostramos informacion del heroe encontrado
+  function limpiar() {
+    $("#mostrar_heroe").hide();
+    $(".resultado").html("");
+    $(".img_heroe").html("");
+    $("#chartContainer").html("");
+  }
+
+  $("button").on("click", function () {
+    let num_heroe = $("#heroe").val(); //recibimos el numero del heroe a buscar
+    let validacion = validar(num_heroe); //validamos que sea numero
+    if (validacion == true) {
+      limpiar();
+      $.ajax({
+        type: "GET",
+        url:
+          "https://www.superheroapi.com/api.php/4905856019427443/" + num_heroe,
+        dataType: "json",
+        success: function (response) {
+          console.log(response);
+          console.log(response.error);
+          if (response.error == "invalid id") {
+            alert(
+              "Número de SuperHero no encontrado : " +
+                response.error +
+                "\nIngrese un numero entre 1 y 731"
+            );
+          } else {
+            //desplegamos la imagen del superhero
+            $(".img_heroe")
+              .append(`<img src="${response.image.url}" class="img-fluid rounded-start" alt="${response.name}"></img>
+      `);
+            //desplegamos los datos del superhero
+            $(".resultado").append(`
+        <h5 class="card-title">Nombre: ${response.name}</h5>
+        <p class="card-text">Conexiones: ${response.connections["group-affiliation"]}</p>
+        <p class="card-text "><small><em>Publicado por:</em> ${response.biography.publisher}</small></p>
+        <p class="card-text border-top"><small><em>Ocupación:</em> ${response.work.occupation}</small></p>
+        <p class="card-text border-top"><small><em>Primera Aparición:</em> ${response.biography["first-appearance"]}</small></p>
+        <p class="card-text border-top"><small><em>Altura:</em> ${response.appearance.height}</small></p>
+        <p class="card-text border-top"><small><em>Peso:</em> ${response.appearance.weight}</small></p>
+        <p class="card-text border-top"><small><em>Alianzas:</em> ${response.biography.aliases}</small></p>
+        `);
+            //desplegamos el grafico del superhero
+            let dataPoints = []; //inicializamos el arreglo con coordenadas para el grafico
+            let grafico_torta = {
+              title: { text: "Estadísticas de Poder para " + response.name },
+              data: [
+                {
+                  type: "pie",
+                  indexLabel: "{name} ({y})",
+                  showInLegend: true,
+                  dataPoints: dataPoints,
+                },
+              ],
+            };
+
+            // comprobamos que no sean nulo los valores de x axis - y axis del grafico
+            if (response.powerstats.intelligence != "null") {
+              dataPoints.push({
+                name: "intelligence",
+                y: response.powerstats.intelligence,
+              });
+            }
+            if (response.powerstats.strength != "null") {
+              dataPoints.push({
+                name: "strength",
+                y: response.powerstats.strength,
+              });
+            }
+            if (response.powerstats.speed != "null") {
+              dataPoints.push({ name: "speed", y: response.powerstats.speed });
+            }
+            if (response.powerstats.durability != "null") {
+              dataPoints.push({
+                name: "durability",
+                y: response.powerstats.durability,
+              });
+            }
+            if (response.powerstats.power != "null") {
+              dataPoints.push({ name: "power", y: response.powerstats.power });
+            }
+            if (response.powerstats.combat != "null") {
+              dataPoints.push({
+                name: "combat",
+                y: response.powerstats.combat,
+              });
+            }
+            //mostramos informacion de heroe
+            $("#mostrar_heroe").show(); //datos
+            $("#chartContainer").CanvasJSChart(grafico_torta); //grafico
+          } //new
+        },
+        error: function (error) {
+          console.log(error);
+          console.log("error", error.status);
+        },
+      });
+    } //new
+  });
+});
